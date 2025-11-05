a2ab7c90bb504a4b614370c8bdfe7c43b53c59c1
Davi Milioli <davimilioli2108@gmail.com>
2025-11-04 21:08:04 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'to-do-list';
+
+  constructor() {
+    this.toggleTheme(true);
+  }
+
+  toggleTheme(isDark: boolean) {
+    const body = document.body;
+    if (isDark) {
+      body.classList.add('dark-theme');
+    } else {
+      body.classList.remove('dark-theme');
+    }
+  }
 }
